0381daeaa429f26f1c0dea20d69b28cb042af0c9
Shane Blowes <shane@idivmac24.idiv.de>
2018-01-23 13:48:18 +0100

    add preliminary analyses of bird data

--- R/birdData_analysis.R
+++ R/birdData_analysis.R
@@ -4,9 +4,11 @@ rm(list=ls())
 # mobr
 library(mobr)
 library(ade4)
+library(tidyverse)
 
 # bird data
-load('')
+load(url('https://github.com/sablowes/MLU_teaching2018/raw/master/data/birdData.Rdata'))
+
 
 # fix rownames of community matrix (to avoid warning)
 rownames(aviurba$fau) <- 1:nrow(aviurba$fau)
@@ -44,3 +46,22 @@ plot(birds_contin, trt_group = 'tree', ref_group = 'mixed')
 birds_contin_noise <- get_delta_stats(bird_mob_in, 'noise', ref_group = 'no')
 plot(birds_contin_noise, trt_group = 'yes', ref_group = 'no')
 overlap_effects(birds_contin_noise, trt_group = 'yes')
+
+## looks like something is going on for noise...calculate some summary stats
+noise_summary <- birds_long %>%
+  # remove unobserved species
+  filter(abundance > 0) %>%
+  group_by(site, feed.hab, migratory) %>%
+  summarise(
+    N = sum(abundance),
+    S = n_distinct(species),
+    ENSPIE = vegan::diversity(abundance, index = 'invsimpson'),
+    noise = unique(noise)) %>%
+  ungroup()
+  
+
+noise_summary %>%
+  ggplot() +
+  facet_wrap(~feed.hab, scales = 'free') +
+  geom_boxplot(aes(x = interaction(noise, migratory), y = N)) +
+  theme_bw()
